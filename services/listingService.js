a82ea111b8106f370f60b2e281ccd0585dfcb1dc
yigitaksoy <yigitaksoy@gmail.com>
2023-05-31 13:30:17 +0200

    Disable stealth

--- services/listingService.js
+++ services/listingService.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+// const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 // const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 
-puppeteer.use(StealthPlugin());
+// puppeteer.use(StealthPlugin());
 // puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
 const scrapeListings = async (url) => {
