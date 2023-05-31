37134fd25b1ab435b72d2bf10c93ca0142894b5d
yigitaksoy <yigitaksoy@gmail.com>
2023-05-31 03:45:05 +0200

    Use puppeteer instead of puppeteer-extra

--- services/listingService.js
+++ services/listingService.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer-extra");
+const puppeteer = require("puppeteer");
 // const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 // const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 
