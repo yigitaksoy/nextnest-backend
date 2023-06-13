dfa9737e96cc8a894ea83175f1a20e057f6db7e6
yigitaksoy <yigitaksoy@gmail.com>
2023-06-13 20:10:28 +0200

    Update api with cron job for test

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cron = require("node-cron");
 const { scrapeListings } = require("../services/listingService");
 const sendEmail = require("../config/email");
 const { v4: uuidv4 } = require("uuid");
@@ -7,82 +8,89 @@ const Listing = require("../models/listing");
 
 const router = express.Router();
 
-router.get("/scrape-listings", async (req, res) => {
-  try {
-    const {
-      listingType,
-      location,
-      minPrice,
-      maxPrice,
-      minSize,
-      minBedrooms,
-      email,
-    } = req.query;
+const processListings = async (user) => {
+  const {
+    listingType,
+    location,
+    minPrice,
+    maxPrice,
+    minSize,
+    minBedrooms,
+    email,
+  } = user.userSearch;
 
-    const listingTypeDutch = listingType === "huur" ? "huur" : "koop";
-    const url = `https://www.funda.nl/en/${listingTypeDutch}/${location}/beschikbaar/${minPrice}-${maxPrice}/${minSize}+woonopp/${minBedrooms}+kamers/1-dag/`;
+  const listingTypeDutch = listingType === "huur" ? "huur" : "koop";
+  const url = `https://www.funda.nl/en/${listingTypeDutch}/${location}/beschikbaar/${minPrice}-${maxPrice}/${minSize}+woonopp/${minBedrooms}+kamers/1-dag/`;
 
-    console.log(`Started scraping listings for URL: ${url}`);
-    const scrapedListings = await scrapeListings(url);
+  console.log(`Started scraping listings for URL: ${url}`);
+  const scrapedListings = await scrapeListings(url);
 
-    const userId = req.user.uid;
-    const user = await User.findOne({ uid: userId });
-    const newScrapedListings = scrapedListings.filter(
-      (listing) =>
-        !user.userListings.some(
-          (userListing) => userListing.title === listing.title
-        )
+  const newScrapedListings = scrapedListings.filter(
+    (listing) =>
+      !user.userListings.some(
+        (userListing) => userListing.title === listing.title
+      )
+  );
+
+  if (newScrapedListings.length > 0) {
+    console.log(
+      `Scraped ${newScrapedListings.length} new listings. Preparing to send email to ${email}`
     );
 
-    if (newScrapedListings.length > 0) {
-      console.log(
-        `Scraped ${newScrapedListings.length} new listings. Preparing to send email to ${email}`
-      );
+    sendEmail(email, newScrapedListings);
+    console.log("Email sent");
+  } else {
+    console.log("No new listings to send email about.");
+  }
 
-      sendEmail(email, newScrapedListings);
-      console.log("Email sent");
+  for (const listing of newScrapedListings) {
+    const listingId = uuidv4();
+
+    // Check if a listing with the same title already exists in the "Listing" collection
+    const existingListingInListings = await Listing.findOne({
+      title: listing.title,
+    });
+
+    // If the listing does not exist in the "Listing" collection, add it
+    if (!existingListingInListings) {
+      let newListing = new Listing(listing);
+      await newListing.save();
     } else {
-      console.log("No new listings to send email about.");
-    }
-
-    for (const listing of newScrapedListings) {
-      const listingId = uuidv4();
-
-      // Check if a listing with the same title already exists in the "Listing" collection
-      const existingListingInListings = await Listing.findOne({
-        title: listing.title,
-      });
-
-      // If the listing does not exist in the "Listing" collection, add it
-      if (!existingListingInListings) {
-        let newListing = new Listing(listing);
-        await newListing.save();
-      } else {
-        console.log(
-          `Listing "${listing.title}" already exists in Listings collection.`
-        );
-      }
-
-      // Check if a listing with the same title already exists in the user's "userListings" array
-      const existingListingInUserListings = user.userListings.find(
-        (userListing) => userListing.title === listing.title
+      console.log(
+        `Listing "${listing.title}" already exists in Listings collection.`
       );
-
-      // If the listing does not exist in the user's "userListings" array, add it
-      if (!existingListingInUserListings) {
-        console.log("Updating user's userListings in the database");
-        await User.findOneAndUpdate(
-          { uid: userId },
-          { $push: { userListings: listing } },
-          { upsert: true, new: true }
-        );
-        console.log("Successfully updated user's userListings");
-      } else {
-        console.log(
-          `Listing "${listing.title}" already exists in user's userListings.`
-        );
-      }
     }
+
+    // Check if a listing with the same title already exists in the user's "userListings" array
+    const existingListingInUserListings = user.userListings.find(
+      (userListing) => userListing.title === listing.title
+    );
+
+    // If the listing does not exist in the user's "userListings" array, add it
+    if (!existingListingInUserListings) {
+      console.log("Updating user's userListings in the database");
+      await User.findOneAndUpdate(
+        { uid: user.uid },
+        { $push: { userListings: listing } },
+        { upsert: true, new: true }
+      );
+      console.log("Successfully updated user's userListings");
+    } else {
+      console.log(
+        `Listing "${listing.title}" already exists in user's userListings.`
+      );
+    }
+  }
+
+  return newScrapedListings;
+};
+
+router.get("/scrape-listings", async (req, res) => {
+  try {
+    const userId = req.user.uid;
+    const user = await User.findOne({ uid: userId });
+
+    const newScrapedListings = await processListings(user);
 
     res.json({ listings: newScrapedListings });
   } catch (error) {
@@ -90,6 +98,15 @@ router.get("/scrape-listings", async (req, res) => {
     res
       .status(500)
       .json({ error: "An error occurred while processing your request" });
+  }
+});
+
+// Schedule tasks to be run on the server.
+cron.schedule("*/15 * * * *", async function () {
+  console.log("Running a job at 01:00 at America/Sao_Paulo timezone");
+  const users = await User.find({});
+  for (const user of users) {
+    await processListings(user);
   }
 });
 
