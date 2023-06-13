736c751d9e074e52e4e82c998505ff0f9b05b0a6
yigitaksoy <yigitaksoy@gmail.com>
2023-06-13 17:15:38 +0200

    Check for duplicates in collections

--- routes/api.js
+++ routes/api.js
@@ -24,33 +24,67 @@ router.get("/scrape-listings", async (req, res) => {
 
     console.log(`Started scraping listings for URL: ${url}`);
     const scrapedListings = await scrapeListings(url);
-    console.log(`Scraped listings for user ${email}:`, scrapedListings);
-    console.log(
-      `Scraped ${scrapedListings.length} listings. Preparing to send email to ${email}`
-    );
-
-    sendEmail(email, scrapedListings);
-
-    console.log("Email sent");
 
     const userId = req.user.uid;
+    const user = await User.findOne({ uid: userId });
+    const newScrapedListings = scrapedListings.filter(
+      (listing) =>
+        !user.userListings.some(
+          (userListing) => userListing.title === listing.title
+        )
+    );
 
-    for (const listing of scrapedListings) {
-      const listingId = uuidv4();
-
-      let newListing = new Listing(listing);
-      await newListing.save();
-
-      console.log("Updating user's userListings in the database");
-      await User.findOneAndUpdate(
-        { uid: userId },
-        { $push: { userListings: listing } },
-        { upsert: true, new: true }
+    if (newScrapedListings.length > 0) {
+      console.log(
+        `Scraped ${newScrapedListings.length} new listings. Preparing to send email to ${email}`
       );
-      console.log("Successfully updated user's userListings");
+
+      sendEmail(email, newScrapedListings);
+      console.log("Email sent");
+    } else {
+      console.log("No new listings to send email about.");
     }
 
-    res.json({ listings: scrapedListings });
+    for (const listing of newScrapedListings) {
+      const listingId = uuidv4();
+
+      // Check if a listing with the same title already exists in the "Listing" collection
+      const existingListingInListings = await Listing.findOne({
+        title: listing.title,
+      });
+
+      // If the listing does not exist in the "Listing" collection, add it
+      if (!existingListingInListings) {
+        let newListing = new Listing(listing);
+        await newListing.save();
+      } else {
+        console.log(
+          `Listing "${listing.title}" already exists in Listings collection.`
+        );
+      }
+
+      // Check if a listing with the same title already exists in the user's "userListings" array
+      const existingListingInUserListings = user.userListings.find(
+        (userListing) => userListing.title === listing.title
+      );
+
+      // If the listing does not exist in the user's "userListings" array, add it
+      if (!existingListingInUserListings) {
+        console.log("Updating user's userListings in the database");
+        await User.findOneAndUpdate(
+          { uid: userId },
+          { $push: { userListings: listing } },
+          { upsert: true, new: true }
+        );
+        console.log("Successfully updated user's userListings");
+      } else {
+        console.log(
+          `Listing "${listing.title}" already exists in user's userListings.`
+        );
+      }
+    }
+
+    res.json({ listings: newScrapedListings });
   } catch (error) {
     console.error("Error occurred in /scrape-listings route:", error);
     res
