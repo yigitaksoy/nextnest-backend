79be1885e54254533b98e7dabedfc32f4314f2b3
yigitaksoy <yigitaksoy@gmail.com>
2023-06-27 19:37:07 +0200

    Improve cron logic and separate timelines

--- services/listingSync.js
+++ services/listingSync.js
@@ -1,9 +1,14 @@
 const cron = require("node-cron");
 const { syncListings } = require("../controllers/apiController");
 
-// Schedule the job to run every 15 minutes between 7 am and 2 am
-cron.schedule("*/5 7-23,0 * * *", async function () {
-  console.log("Running a job every 15 minutes between 7 am and 2 am.");
+async function syncJob() {
+  console.log("Running a job every 5 minutes between 7 am and 2 am.");
   await syncListings();
   console.log("Done! Thank you for using NextNest! 🚀");
-});
+}
+
+// Schedule the job to run every 5 minutes between 7 am and 11:59 pm
+cron.schedule("*/5 7-23 * * *", syncJob);
+
+// Schedule the job to run every 5 minutes between 12 am and 2 am
+cron.schedule("*/5 0-2 * * *", syncJob);
