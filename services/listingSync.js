431b97295b43dd0a09ea605923320b6fe7be61fd
yigitaksoy <yigitaksoy@gmail.com>
2023-06-27 18:54:45 +0200

    Schedule the job to run every 15 minutes between 7 am and 2 am

--- services/listingSync.js
+++ services/listingSync.js
@@ -1,8 +1,9 @@
 const cron = require("node-cron");
 const { syncListings } = require("../controllers/apiController");
 
-cron.schedule("*/5 * * * *", async function () {
-  console.log("Running a job every 15 minutes.");
+// Schedule the job to run every 15 minutes between 7 am and 2 am
+cron.schedule("*/15 7-13,0-1 * * *", async function () {
+  console.log("Running a job every 15 minutes between 7 am and 2 am.");
   await syncListings();
   console.log("Done! Thank you for using NextNest! 🚀");
 });
