37df1e486c6a3f1941875fca17a12097de07e8ce
yigitaksoy <yigitaksoy@gmail.com>
2023-06-27 14:39:15 +0200

    Implemented retry logic

--- services/listingDetails.js
+++ services/listingDetails.js
@@ -2,7 +2,21 @@ const listingDetails = async (page, url, listingType) => {
   console.log("Scraping details for URL:", url);
 
   try {
-    await page.goto(url, { waitUntil: "domcontentloaded" });
+    let navigationAttempts = 0;
+    while (navigationAttempts < 5) {
+      // Maximum 5 attempts
+      try {
+        await page.goto(url, { waitUntil: "domcontentloaded", timeout: 60000 });
+        break; // break the loop if navigation succeeds
+      } catch (error) {
+        navigationAttempts++;
+        console.error(
+          `Error during navigation attempt ${navigationAttempts}: ${error}`
+        );
+        if (navigationAttempts >= 5) throw error; // if all attempts fail, throw the error
+        await page.waitForTimeout(5000); // wait for 5 seconds before next attempt
+      }
+    }
 
     // Wait for a random amount of time between 0 to 10 seconds
     await page.waitForTimeout(Math.random() * 10000);
@@ -11,46 +25,54 @@ const listingDetails = async (page, url, listingType) => {
     const title = await page.title();
     console.log("Page Title:", title);
 
-    const rawDetails = await page.evaluate(() => {
-      const details = {};
-      const dtElements = document.querySelectorAll(".object-kenmerken-list dt");
-      dtElements.forEach((dt) => {
-        const dd = dt.nextElementSibling;
-        if (dd && dd.tagName.toLowerCase() === "dd") {
-          let label = dt.textContent.trim().split("\n")[0];
-          let valueElement = dd.querySelector("span");
-          let value = valueElement
-            ? valueElement.textContent
-                .trim()
-                .split("\n")
-                .map((str) => str.trim())
-                .join(" ")
-            : undefined;
-          details[label] = value;
+    let rawDetails;
+    try {
+      rawDetails = await page.evaluate(() => {
+        const details = {};
+        const dtElements = document.querySelectorAll(
+          ".object-kenmerken-list dt"
+        );
+        dtElements.forEach((dt) => {
+          const dd = dt.nextElementSibling;
+          if (dd && dd.tagName.toLowerCase() === "dd") {
+            let label = dt.textContent.trim().split("\n")[0];
+            let valueElement = dd.querySelector("span");
+            let value = valueElement
+              ? valueElement.textContent
+                  .trim()
+                  .split("\n")
+                  .map((str) => str.trim())
+                  .join(" ")
+              : undefined;
+            details[label] = value;
+          }
+        });
+
+        // Extract the neighborhood
+        const breadcrumbElements =
+          document.querySelectorAll(".breadcrumb-list a");
+        const neighborhoodElement =
+          breadcrumbElements[breadcrumbElements.length - 1];
+        if (neighborhoodElement) {
+          details["Neighborhood"] = neighborhoodElement.textContent.trim();
         }
+
+        // Extract the price per m²
+        const pricePerM2Element = document.querySelector(
+          ".object-kenmerken-list__asking-price"
+        );
+        if (pricePerM2Element) {
+          details["Asking price per m²"] = pricePerM2Element.textContent
+            .trim()
+            .split("\n")[0];
+        }
+
+        return details;
       });
-
-      // Extract the neighborhood
-      const breadcrumbElements =
-        document.querySelectorAll(".breadcrumb-list a");
-      const neighborhoodElement =
-        breadcrumbElements[breadcrumbElements.length - 1];
-      if (neighborhoodElement) {
-        details["Neighborhood"] = neighborhoodElement.textContent.trim();
-      }
-
-      // Extract the price per m²
-      const pricePerM2Element = document.querySelector(
-        ".object-kenmerken-list__asking-price"
-      );
-      if (pricePerM2Element) {
-        details["Asking price per m²"] = pricePerM2Element.textContent
-          .trim()
-          .split("\n")[0];
-      }
-
-      return details;
-    });
+    } catch (error) {
+      console.error("Error during page evaluation:", error);
+      // Implement your own error handling logic here...
+    }
 
     // Structure the data
     const details = {
