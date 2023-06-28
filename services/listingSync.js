b50a59ccd4c5747daa7efa08e05858caa63fe5a5
yigitaksoy <yigitaksoy@gmail.com>
2023-06-29 01:10:23 +0200

    Update listing sync schedule

--- services/listingSync.js
+++ services/listingSync.js
@@ -7,8 +7,11 @@ async function syncJob() {
   console.log("Done! Thank you for using NextNest! 🚀");
 }
 
-// Schedule the job to run every 10 minutes between 7 am and 11:59 pm
-cron.schedule("*/10 7-23 * * *", syncJob);
+// Schedule the job to run every 10 minutes between 7 am and 08:59 pm
+cron.schedule("*/10 7-20 * * *", syncJob);
 
-// Schedule the job to run every 10 minutes between 12 am and 2 am
-cron.schedule("*/10 0-2 * * *", syncJob);
+// Schedule the job to run every 15 minutes between 08:59 pm and 11:59 pm
+cron.schedule("*/15 20-23 * * *", syncJob);
+
+// Schedule the job to run every 30 minutes between 12 am and 2 am
+cron.schedule("*/30 0-2 * * *", syncJob);
