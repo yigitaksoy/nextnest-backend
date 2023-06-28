83cd6ab22af6ae38983fc446d8fda492c8388446
yigitaksoy <yigitaksoy@gmail.com>
2023-06-29 01:32:44 +0200

    Implement error handling

--- services/listingSync.js
+++ services/listingSync.js
@@ -1,10 +1,18 @@
 const cron = require("node-cron");
 const { syncListings } = require("../controllers/apiController");
 
-async function syncJob() {
-  console.log("Running a job every 10 minutes between 7 am and 2 am.");
-  await syncListings();
-  console.log("Done! Thank you for using NextNest! 🚀");
+function syncJob() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      console.log("Sync in progress... 📡  ");
+      await syncListings();
+      console.log("Done! Thank you for using NextNest! 🚀");
+      resolve();
+    } catch (error) {
+      console.error("⛔ Synchronization Error: ", error);
+      reject(error);
+    }
+  });
 }
 
 // Schedule the job to run every 10 minutes between 7 am and 08:59 pm
